4abbe78d14d5fc8676ed7da4977dae52467a5650
Lee Lindsay <zedhex81@gmail.com>
2020-06-23 20:23:24 +0100

    Added link to github repository

--- app/views/pages/about.php
+++ app/views/pages/about.php
@@ -2,5 +2,5 @@
   <h1>About</h1>
   <p>This is a social network type app built on the TraversyMVC framework</p>
   <p>App Version: <?php echo $data['version']; ?></p>
-  <a href="<?php echo URLROOT; ?>/pages/about"><h4>View source code</h4></a>
+  <a href="https://github.com/A-N-Other92/shareposts" target="_blank"><h4>View source code</h4></a>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
